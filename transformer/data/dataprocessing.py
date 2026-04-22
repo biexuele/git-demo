5dda62289ed8efa5d5caf145c8c353519da8e2e6
biexuele <1452359863@qq.com>
2026-04-22 18:36:21 +0800

    分词

--- transformer/data/dataprocessing.py
+++ transformer/data/dataprocessing.py
@@ -30,7 +30,31 @@ class TRANS(Dataset):
 
 data = TRANS('data/translation2019zh/translation2019zh_train.json')
 train_data, valid_data = random_split(data, [train_set_size, valid_set_size])
-
-
-
 test_data = TRANS('data/translation2019zh/translation2019zh_valid.json')
+
+print(f'train set size: {len(train_data)}')
+print(f'valid set size: {len(valid_data)}')
+print(f'test set size: {len(test_data)}')
+print(next(iter(train_data)))
+
+'''
+通过 DataLoader 库来按 batch 加载数据，将文本转换为模型可以接受的 token IDs。
+'''
+from transformers import AutoTokenizer
+
+# 汉英翻译模型 opus-mt-zh-en 对应的分词器，源语言为中文
+model_checkpoint = "Helsinki-NLP/opus-mt-zh-en"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+zh_sentence = train_data[0]["chinese"]
+en_sentence = train_data[0]["english"]
+
+inputs = tokenizer(zh_sentence)
+# 要编码目标语言（英语）则需要使用 text_targets 参数
+# 如果忘记使用 text_targets 参数，就会使用源语言分词器对目标语言进行编码，产生糟糕的分词结果
+targets = tokenizer(text_target=en_sentence)
+
+wrong_targets = tokenizer(en_sentence)
+print(tokenizer.convert_ids_to_tokens(inputs["input_ids"]))
+print(tokenizer.convert_ids_to_tokens(targets["input_ids"]))
+print(tokenizer.convert_ids_to_tokens(wrong_targets["input_ids"]))
